21cc5df71826be44fa2484506f19bce2340fd2d0
mayeonn <82192918+mayeonn@users.noreply.github.com>
2021-05-22 23:51:26 +0900

    Update UsingBufferedReader.java

--- src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
+++ src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
@@ -13,8 +13,10 @@ public class UsingBufferedReader {
 
         StopWatch.start(); 
 
-        BufferedReader inputStream= new BufferedReader(new InputStreamReader(System.in));
-        while (inputStream.read()!=-1){} 
+        BufferedReader inputStream= new BufferedReader(args);
+        
+        while (args.read()!=-1){} 
+        
 
         long duration = StopWatch.stop();
         System.out.println(duration + " milsec");
@@ -23,17 +25,17 @@ public class UsingBufferedReader {
 
 
         //-------------- Test reading 10 MB file. --------------------
-
+/*
         StopWatch.start();
 
         BufferedReader inputStream2= new BufferedReader(new InputStreamReader(System.in));
-        while (inputStream2.read()!=-1){}
+        while (inputStream2.read()!=-1){} 
 
         long duration2 = StopWatch.stop();
         System.out.println(duration2 + " milsec"); 
 
         inputStream2.close();
-
+*/
         /*
         //-------------- Test reading 100 MB file. --------------------
 
